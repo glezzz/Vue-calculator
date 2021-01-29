877f260df68a8c4f4a111612cf861a4b75bdb26d
glezzz <alexglezk@gmail.com>
2021-01-29 11:41:37 +0100

    initial commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="calculator">
+    <div>hello</div>
+    <div>hello</div>
+    <div>hello</div>
+    <div>hello</div>
+    <div>hello</div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.calculator {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: minmax(50px, auto);
+}
+</style>
